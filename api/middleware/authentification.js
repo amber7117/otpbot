4205011d5b0f3b14b8280b2b75782cd864146efc
amber7117 <90747456+amber7117@users.noreply.github.com>
2025-01-24 03:01:47 +0800

    Update authentification.js

--- api/middleware/authentification.js
+++ api/middleware/authentification.js
@@ -1,47 +1,48 @@
 module.exports = (request, response, next) => {
   /**
-   * Fichier contenant les configurations nécéssaires au bon fonctionnement du système
+   * File containing necessary configurations for system functionality.
    */
   const config = require('../config');
 
   try {
-      /**
-       * Ici l'on récupère le mot de passe de l'api, par requête GET ou POST
-       */
-      const pass = request.body.password || request.params.apipassword;
+    /**
+     * Here, we get the API password via GET or POST request.
+     */
+    const pass = request.body.password || request.params.apipassword;
 
-      /**
-       * On vérifie si le mot de passe de l'api n'est pas vide, si oui, erreur de sécurité et on retourne une erreur
-       */
-      if (config.apipassword == '') error('Your API Password is not set, look at your config file.', 401);
+    /**
+     * Check if the API password is set; if not, throw a security error and return an error response.
+     */
+    if (config.apipassword === '') {
+      throwError('Your API Password is not set. Check your config file.', 401);
+    }
 
-      /**
-       * Selon les cas d'utilisation, l'on renvoit un code d'erreur où alors l'on next() => signifie que tout est bon et que la fonction passe
-       */
-      switch (pass) {
-          case ' ':
-              error('The password you sent is empty.', 401);
-              break;
-          case undefined:
-              error('Please send the password API.', 401);
-              break;
-          case config.apipassword:
-              next();
-              break;
-          default:
-              error('Invalid password.', 401);
-              break;
-      }
-
+    /**
+     * Depending on the cases, we either return an error code or proceed with next() if everything is fine.
+     */
+    switch (pass) {
+      case ' ':
+        throwError('The password you sent is empty.', 401);
+        break;
+      case undefined:
+        throwError('Please send the API password.', 401);
+        break;
+      case config.apipassword:
+        next();
+        break;
+      default:
+        throwError('Invalid password.', 401);
+        break;
+    }
   } catch {
-      response.status(401).json({
-          error: 'Invalid request!'
-      });
+    response.status(401).json({
+      error: 'Invalid request!'
+    });
   }
 
-  function error(msg, statuscode) {
-      response.status(statuscode).json({
-          error: msg
-      });
+  function throwError(msg, statuscode) {
+    response.status(statuscode).json({
+      error: msg
+    });
   }
 };
